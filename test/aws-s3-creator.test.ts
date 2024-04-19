49ab0fe71d84e327808d4e54f219074099bddd55
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-19 22:52:09 +0700

    Init project

---
+++ test/aws-s3-creator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsS3Creator from '../lib/aws-s3-creator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-s3-creator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsS3Creator.AwsS3CreatorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
