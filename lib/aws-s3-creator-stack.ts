6f860b9df2d65e625c104cade4de858ced7d3c1f
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-20 15:16:14 +0700

    Init AwsS3CreatorStack

--- lib/aws-s3-creator-stack.ts
+++ lib/aws-s3-creator-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { AwsS3CreatorStackProps } from './AwsS3CreatorStackProps';
 
 export class AwsS3CreatorStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: AwsS3CreatorStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const removalPolicy = props.deployEnvironment === 'production' ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY;
+    const bucketName = `${props.resourcePrefix}-${props.s3BucketName}`;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsS3CreatorQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // define an S3 bucket
+    const s3Bucket = new s3.Bucket(this, bucketName, {
+      bucketName: bucketName,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: false,
+      removalPolicy: removalPolicy,
+      autoDeleteObjects: removalPolicy === cdk.RemovalPolicy.DESTROY,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      versioned: true, // Enable versioning
+    });
+
+    // export s3Bucket name
+    new cdk.CfnOutput(this, `${props.appName}-${props.deployEnvironment}-s3BucketName`, {
+      value: s3Bucket.bucketName,
+      exportName: `${props.appName}-${props.deployEnvironment}-s3BucketName`,
+      description: 'The name of the S3 bucket.',
+    });
   }
 }
