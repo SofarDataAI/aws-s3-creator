202281312d2764aa4fe642ec3f09ae281ae995cd
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-20 15:06:46 +0700

    Add AwsS3CreatorStackProps interface

---
+++ lib/AwsS3CreatorStackProps.ts
@@ -0,0 +1,20 @@
+import { StackProps } from "aws-cdk-lib";
+
+export interface AwsS3CreatorStackProps extends StackProps {
+    /**
+     * A prefix used for naming resources to ensure uniqueness across deployments.
+     */
+    readonly resourcePrefix: string;
+    /**
+     * The AWS region where resources will be deployed. Can be undefined for default region.
+     */
+    readonly deployRegion: string | undefined;
+    /**
+     * The deployment environment (e.g., dev, prod) for resource tagging and logical separation.
+     */
+    readonly deployEnvironment: string;
+    /**
+     * The name of the application, used for resource naming and tagging.
+     */
+    readonly appName: string;
+}
